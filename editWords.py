1bbe29a0bf2372e5b533f699a60009becdd8338b
Joshua Park <wljoshpark@gmail.com>
2022-01-31 18:42:32 -0500

    add graphics library, update word list, and start working on game

--- editWords.py
+++ editWords.py
@@ -2,12 +2,9 @@ from os import rmdir
 
 
 with open('words.txt', 'r') as f:
-    lines = f.readlines()
-with open('wordsNew.txt', 'w') as f:
-    for line in lines:
-        line = line.strip(' ').strip('\n')
-        print(line)
-        if line == "\n" or len(line) != 5:
-            pass
-        else:
-            f.write(line + '\n')
+    lines = sorted(set(f.readlines()))
+with open('words.txt', 'w') as f:
+    for word in lines:
+        word = word.lower()
+        if len(word) == 6:
+            f.write(word)
--- game.py
+++ game.py
@@ -1,12 +1,40 @@
 import os
 import random
 
+from matplotlib import lines
+from graphics import *
+
 dirname = os.path.dirname(__file__)
-n = random.randint(0, 3020)
+n = random.randint(0, 2612)
 
 with open(f"{dirname}/words.txt") as f:
-    lines = set(f.readlines())
-    lines = sorted(lines)
-    word = lines.pop(n).lower()
-    print(word)
+    lines = sorted(set(f.readlines()))
+    for word in lines:
+        i = lines.index(word)
+        lines[i] = word.strip()
+    word = lines.pop(n)
 
+word = [char for char in word]
+print(word)
+window = GraphWin("Wurdle", 500, 600)
+
+guess = [char for char in input("guess ")]
+
+for i in range(len(word)):
+    xPos = 70 + (i * 90)
+    yPos = 50
+
+    rect = Rectangle(Point(110+(i*90), 90), Point(30+(i*90), 10))
+    if guess[i] == word[i]:
+        rect.setFill("green")
+    elif guess[i] in word:
+        rect.setFill("yellow")
+    else:
+        rect.setFill("grey")
+    rect.draw(window)
+
+    letter = Text(Point(xPos, yPos), guess[i])
+    letter.setSize(82)
+    letter.draw(window)
+
+input("Press any key to exit... ")
